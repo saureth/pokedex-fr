ac51665394d503304c015179a7fa45bedf51a43f
Santiago Sanmartín <saureth001@gmail.com>
2023-11-07 19:38:09 -0500

    feat: initial modules

---
+++ src/app/feature/feature.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PokedexComponent } from './pokedex/pokedex.component';
+
+
+
+@NgModule({
+  declarations: [
+    PokedexComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FeatureModule { }
