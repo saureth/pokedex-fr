a40635c392fa61befd5e99b1f88f0d6b0cc9d71b
Santiago Sanmartín <saureth001@gmail.com>
2023-11-13 21:09:40 -0500

    feat: Created forkjoin service for related pokemons

--- src/app/core/service-template/https.service.ts
+++ src/app/core/service-template/https.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, forkJoin, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +9,31 @@ export class HttpsService {
 
   POKEMON_ENV = "https://pokeapi.co/api/v2/pokemon/";
   DEFAULT_PKM = "charizard";
+  related: any = {};
 
   constructor(private http: HttpClient) { }
 
   getPokemon (base: string = this.POKEMON_ENV, pokemon: string = this.DEFAULT_PKM) {
     let finalUrl = base.concat(pokemon);
     return this.http.get(finalUrl);
+  }
+
+  forkPokemon(base: string = this.POKEMON_ENV, pokemons: any){
+    forkJoin([
+      this.getPokemon(base,pokemons.one),
+      this.getPokemon(base,pokemons.two),
+      this.getPokemon(base,pokemons.three),
+      this.getPokemon(base,pokemons.four)
+    ]).subscribe({
+      next: (results) => {
+        this.related = {
+          one: results[0],
+          two: results[1],
+          three: results[2],
+          four: results[3]
+        };
+      }
+    })
   }
 
 }
